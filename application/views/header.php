9d9152beb12db744f354b6fb6423d30ea26a4cc8
devanastiko <devan.astiko@gmail.com>
2016-03-11 23:55:54 +0700

    backup

---
+++ application/views/header.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Basic Page Needs
+    ================================================== -->
+    <meta charset="utf-8">
+    <!--[if IE]><meta http-equiv="x-ua-compatible" content="IE=9" /><![endif]-->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Welcome || Stander.id</title>
+    <meta name="description" content="Your Description Here">
+    <meta name="keywords" content="bootstrap themes, portfolio, responsive theme">
+    <meta name="author" content="ThemeForces.Com">
+    
+    <!-- Favicons
+    ================================================== -->
+    <link rel="shortcut icon" href="<?php echo base_url();?>public/img/favicon.ico" type="image/x-icon">
+    <link rel="apple-touch-icon" href="img/apple-touch-icon.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="img/apple-touch-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="img/apple-touch-icon-114x114.png">
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" type="text/css"  href="<?php echo base_url();?>public/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>public/fonts/font-awesome/css/font-awesome.css">
+
+    <!-- Stylesheet
+    ================================================== -->
+    <link rel="stylesheet" type="text/css"  href="<?php echo base_url();?>public/css/style.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>public/css/responsive.css">
+
+    <script type="text/javascript" src="<?php echo base_url();?>public/js/modernizr.custom.js"></script>
+
+    <link href='http://fonts.googleapis.com/css?family=Raleway:500,600,700,100,800,900,400,200,300' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Playball' rel='stylesheet' type='text/css'>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+ <body>
---
+++ application/views/header_header.php
@@ -0,0 +1,43 @@
+    <div id="tf-home">
+        <div class="overlay">
+            <div id="sticky-anchor"></div>
+            <nav id="tf-menu" class="navbar navbar-default">
+                <div class="container">
+                    <!-- Brand and toggle get grouped for better mobile display -->
+                    <div class="navbar-header">
+                      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand logo" href="">Stander.id</a>
+                </div>
+
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                  <ul class="nav navbar-nav navbar-right">
+                    <li><a href="" class="btn btn-primary my-btnheader">Saya Penyedia</a></li>
+                    <li><a href="" class="btn btn-primary my-btnli">Stan</a></li>
+                    <li><a href="" class="btn btn-primary my-btnli">Blog</a></li>
+                    <li><a href="" class="btn btn-primary my-btnli">About</a></li>
+                    <li><a href="" class="btn btn-primary my-btnli">Login</a></li>
+                </ul>
+            </div><!-- /.navbar-collapse -->
+        </div><!-- /.container-fluid -->
+    </nav>
+
+    <div class="container">
+        <div class="content">
+            <h1>Stander.id</h1>
+            <h3>Hi, We delighted to find your recommended stand </h3>
+            <br>
+            <a href="" class="btn btn-primary my-btn">Find Stan</a>
+            <a href="" class="btn btn-primary my-btn2">How It Works</a>
+            
+            
+    </div>
+
+</div>
+</div>
+</div>
